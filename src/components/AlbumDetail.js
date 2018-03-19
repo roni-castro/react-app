34f615c765125abac5e5b592652d263a74385586
Roni <roni.c.castro@gmail.com>
2018-03-19 04:22:56 -0300

    Add Card component and use it

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
 
  const AlbumDetail = (props) => (
-    <View>
+    <Card>
       <Text>{ props.album.title }</Text>
-    </View>
+    </Card>
 );
 
 export default AlbumDetail;
